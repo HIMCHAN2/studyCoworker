1b059b9a7737c3f22b99d9dfe6b9f80aad1cfc90
Him <Him@Him>
2021-12-30 11:12:28 +0900

    "linkedlist 주석 작업 v0.1"

--- study_Coworker/src/studyToLinkedList/StudyToLinkedList.java
+++ study_Coworker/src/studyToLinkedList/StudyToLinkedList.java
@@ -2,6 +2,11 @@ package studyToLinkedList;
 
 public class StudyToLinkedList {
 	public static void main(String[] args) {
-		System.out.println("여기");
+		/*
+		 * LinkedList 란 각 노드가 데이터와 포인터를 가지고 한 줄로 연결되어 있는 방식의 자료구조 
+		 */
+		
+		
+		
 	}
 }
